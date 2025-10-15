dfa4a2696f5141949cd531ee632c495c6a83624b
Clayton <claytonsmith3.14@gmail.com>
2025-10-15 16:29:50 -0600

    lab 4

--- components/tone/tone.c
+++ components/tone/tone.c
@@ -10,12 +10,11 @@
 #define MAX_VALF 255.0F
 #define PI 3.14159
 
-#define FOUR 4 
-#define THREE 3
+#define QUARTER_FREQ 4 
+#define THREE_X_PERIOD 3
 
 #define CLAMP(x,max,min) (((x)<(max))? (((x)>(min))? (x):(min)) : (max))
 #define BIAS_AND_SCALE(x) ((uint8_t)CLAMP(BIAS*((x)+1.0F),MAX_VALF, 0.0F))
-//#define BIAS_AND_SCALE(x) ((uint8_t)((BIAS)*((x)+1.0F)))
 
 // This component is a thin layer around the sound component.
 // One cycle of a waveform is generated and then given to the
@@ -34,9 +33,10 @@ size_t sound_buffer_size;
 // Return zero if successful, or non-zero otherwise.
 int32_t tone_init(uint32_t sample_hz) {
     sample_freq = sample_hz;
+    // make sure frequency is larger than niquist frequency for all samples
     if (sample_hz > (2 * LOWEST_FREQ)) {
         //define buffer
-        sound_buffer_size = sample_hz/LOWEST_FREQ;
+        sound_buffer_size = sample_hz/LOWEST_FREQ+1;
         sound_buffer = (uint8_t*)malloc(sizeof(uint8_t)*sound_buffer_size);
         return sound_init(sample_hz);
     } else {
@@ -58,15 +58,17 @@ void tone_start(tone_t tone, uint32_t freq) {
     if(freq < LOWEST_FREQ) return;
 
     uint32_t num_samples = sample_freq/freq;
-    printf("here! %ld\n", num_samples);
 
+    // loop over each index of sound sample to fill in
     for(int i=0; i<num_samples; i++) {
         float t = 1.0F*i/sample_freq;
-
+        // switch depending on mode of sound production
         switch(tone) {
+            // sine wave
             case SINE_T: {
                 sound_buffer[i] = BIAS_AND_SCALE(sinf(2*PI*freq*t));
             } break;
+            // square wave
             case SQUARE_T: {
                 if(i < (num_samples/2)) {
                     sound_buffer[i] = MAX_VAL;
@@ -74,29 +76,30 @@ void tone_start(tone_t tone, uint32_t freq) {
                     sound_buffer[i] = 0;
                 }
             } break;
+            // triangle wave
             case TRIANGLE_T: {
-                if (i < (num_samples/FOUR)) {
-                    sound_buffer[i] = BIAS_AND_SCALE(t*freq*FOUR);
-                } else if ((num_samples/FOUR<=i) && (i<num_samples*THREE/FOUR)) {
-                    sound_buffer[i] = BIAS_AND_SCALE(2-t*freq*FOUR);
+                if (i < (num_samples/QUARTER_FREQ)) {
+                    sound_buffer[i] = BIAS_AND_SCALE(t*freq*QUARTER_FREQ);
+                } else if ((num_samples/QUARTER_FREQ<=i) && (i<num_samples*THREE_X_PERIOD/QUARTER_FREQ)) {
+                    sound_buffer[i] = BIAS_AND_SCALE(2-t*freq*QUARTER_FREQ);
                 } else {
-                    sound_buffer[i] = BIAS_AND_SCALE(t*freq*FOUR-FOUR);
+                    sound_buffer[i] = BIAS_AND_SCALE(t*freq*QUARTER_FREQ-QUARTER_FREQ);
                 }
             } break;
+            // sawtooth wave
             case SAW_T: {
                 if (i < (num_samples/2)) {
                     sound_buffer[i] = BIAS_AND_SCALE(t*freq*2);
                 } else {
-                    sound_buffer[i] = BIAS_AND_SCALE(t*freq*2.0-2.0);
+                    sound_buffer[i] = BIAS_AND_SCALE(t*freq*2-2);
                 }
             } break;
+            // Nothing
             case LAST_T: {
-                // sound_buffer[i] = 0;
             } break;
             default: break;
         }
-        // printf("%x.", sound_buffer[i]);
     }
 
-    sound_cyclic(sound_buffer, num_samples-1);
+    sound_cyclic(sound_buffer, num_samples);
 }
